4a6ce9ad72093386e970fc018ecf0d600fef661a
赵静波 <zhaojingbo918@163.com>
2020-11-28 18:07:40 +0800

    控制针的连续发射

---
+++ StickPinProject/Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private Transform startPoint;
+    private Transform spawnPoint;
+    public GameObject PinPrefab;
+    public Pin currentPin;
+    // Use this for initialization
+    void Start()
+    {
+        startPoint = GameObject.Find("StartPoint").transform;
+        spawnPoint = GameObject.Find("SpawnPoint").transform;
+
+        this.SpawnPin();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            currentPin.Flay();
+            this.SpawnPin();
+        }
+    }
+
+
+    public void SpawnPin()
+    {
+        Debug.Log("--------spawnPin----------");
+        currentPin = GameObject.Instantiate(PinPrefab, spawnPoint.position, PinPrefab.transform.rotation).GetComponent<Pin>();
+    }
+}
---
+++ StickPinProject/Assets/Scripts/Pin.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour
+{
+    public float speed = 5;
+    private bool isFly;
+    private bool isReach = false;
+    private Transform startPoint;
+    public Transform circle;
+    private Vector3 targetPosition;
+
+    private GameManager gameManager;
+    // Use this for initialization
+    void Start()
+    {
+        startPoint = GameObject.Find("StartPoint").transform;
+        circle = GameObject.Find("Circle").transform;
+        targetPosition = new Vector3(circle.position.x, (float)(circle.position.y - 1.1), circle.position.z);
+
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFly == false)
+        {
+            if (isReach == false)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, startPoint.position, speed * Time.deltaTime);
+
+                if (Vector3.Distance(transform.position, startPoint.position) < 0.05f)
+                {
+                    isReach = true;
+                }
+            }
+        }
+        else
+        {
+
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+            if (Vector3.Distance(transform.position, targetPosition) < 0.05f)
+            {
+                isFly = false;
+                transform.position = targetPosition;
+                transform.parent = circle;
+
+            }
+
+        }
+    }
+
+    public void Flay()
+    {
+        isFly = true;
+        Debug.Log("flay--------");
+    }
+}
